710f9a18a15b40bfeee4d5b0e4cbd17e7f5b1155
Vicky Sharma <vicky.sharma@digitbazar.com>
2020-08-30 18:25:24 +0530

    fb clone completed

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -9,9 +9,15 @@ function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })));
-    });
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => {
+            return { id: doc.id, data: doc.data() };
+          })
+        );
+      });
   }, []);
 
   return (
